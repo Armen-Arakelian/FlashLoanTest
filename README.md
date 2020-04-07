bfc1fc126651935c4752e4a7b0aa89a9cd0b499d
aarakelian <aarakelian@s-pro.io>
2020-04-07 18:24:18 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # FlashLoanTest
+
+Firstly you need to create .env file and fill it according to .env.example file.
+
+For running test script you will need to install packages with
+``` npm install ```
+
+Then run 
+
+``` npm run compile ``` 
+
+to compile all contracts.
+
+And then run 
+
+``` npm run deploy ``` 
+
+to deploy contract and interact with FlashLoan. It will fail for now because of their service issue.
---
+++ contracts/integration/FlashLoanTest.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+import "../flashloan/base/FlashLoanReceiverBase.sol";
+import "../configuration/LendingPoolAddressesProvider.sol";
+import "../lendingpool/LendingPool.sol";
+
+contract FlashLoanTest is FlashLoanReceiverBase, Ownable {
+
+    using SafeMath for uint256;
+
+    LendingPool lendingPool;
+    LendingPoolAddressesProvider provider;
+
+    event Succsess();
+
+    constructor(LendingPoolAddressesProvider _provider)
+        FlashLoanReceiverBase(_provider)
+        public {
+          provider = LendingPoolAddressesProvider(_provider);
+          lendingPool = LendingPool(provider.getLendingPool());
+        }
+
+    function flashLoan() external onlyOwner() {
+      address ethAddress = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+      address receiver = address(this);
+      uint256 amount = 1000 * 1e14;
+      bytes memory params = "";
+
+      lendingPool.flashLoan(receiver, ethAddress, amount, params);
+      emit Succsess();
+    }
+
+    function executeOperation(
+        address _reserve,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _params) external {
+
+        //check the contract has the specified balance
+        require(_amount <= getBalanceInternal(address(this), _reserve), 
+            "Invalid balance for the contract");
+
+        /**
+            CUSTOM ACTION TO PERFORM WITH THE BORROWED LIQUIDITY
+            
+            Example of decoding bytes param of type `address` and `uint`
+            (address sampleAddress, uint sampleAmount) = abi.decode(_params, (address, uint));
+        */
+
+        transferFundsBackToPoolInternal(_reserve, _amount.add(_fee));
+    }
+
+    function () external payable {}
+}
